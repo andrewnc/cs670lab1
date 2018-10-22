225f94bb865d4f62103cb069ea9c31032b0a74b6
andrew <andrewcarr06@gmail.com>
2018-10-22 11:15:44 -0600

    saving current progress

--- lab2/games.py
+++ lab2/games.py
@@ -6,7 +6,7 @@ from agents import *
 
 
 class Game():
-    def __init__(self, row_player, col_player, row_payoff_matrix = [[3, 1], [5, 2]], col_payoff_matrix = [[3, 5], [1, 2]]):
+    def __init__(self, row_player=None, col_player=None, row_payoff_matrix = [[3, 1], [5, 2]], col_payoff_matrix = [[3, 5], [1, 2]]):
         """defaults to prisoner's dilemma"""
         self.row_payoff_matrix = row_payoff_matrix
         self.col_payoff_matrix = col_payoff_matrix
@@ -43,6 +43,11 @@ class Game():
         self.row_payoff.append(row_temp_payoff)
         self.col_payoff.append(col_temp_payoff)
 
+
+    def set_players(self, row_player, col_player):
+        self.row_player = row_player
+        self.col_player = col_player
+
     def get_row_payoff(self):
         return np.sum(self.row_payoff)
     
@@ -65,6 +70,7 @@ def main():
         [0.1, 0.9],
         [0.9, 0.1]
     ]
+
     payoffs = []
 
     prisdel_row_payoff_matrix = [[3, 1], [5, 2]]
@@ -77,19 +83,47 @@ def main():
     battle_col_payoff_matrix = [[0, 2], [1, 0]]
 
     games = [
-        ["Prisoner's Dilemma", prisdel_row_payoff_matrix, prisdel_col_payoff_matrix],
-        ["Stag Hunt", staghunt_row_payoff_matrix, staghunt_col_payoff_matrix],
-        ["Battle of the Sexes", battle_row_payoff_matrix, battle_col_payoff_matrix]
+        ["Prisoner's Dilemma", [prisdel_row_payoff_matrix, prisdel_col_payoff_matrix]],
+        ["Stag Hunt", [staghunt_row_payoff_matrix, staghunt_col_payoff_matrix]],
+        ["Battle of the Sexes", [battle_row_payoff_matrix, battle_col_payoff_matrix]]
     ]
 
     #replicator dynamics
     agent_pairs = list(combinations(agents, 2))
 
-    print(agent_pairs)
+    for game_name, game in games:
+        print(game_name)
+        row_payoff_matrix = game[0]
+        col_payoff_matrix = game[1]
+        for (agent1_name, agent1), (agent2_name, agent2)  in agent_pairs:
+            print("{} vs {}".format(agent1_name, agent2_name))
+            for gamma in gammas:
+                for theta1, theta2 in initial_population_distribution:
+                    payoff = {agent1_name:0, agent2_name: 0}
 
-    for agent1_name, agent1, agent2_name, agent2  in agent_pairs:
-        for theta1, theta2 in initial_population_distribution:
-            pass
+                    # calculate phi for each agent
+                    n_agent1 = int(theta1*n_agents)
+                    n_agent2 = int(theta2*n_agents)
+
+                    # log, initialize, and shuffle agent space
+                    print("{}: {}\n{}: {}".format(n_agent1, agent1_name, n_agent2, agent2_name))
+                    current_agents_generation = [agent1() for x in range(n_agent1)]+[agent2() for x in range(n_agent2)]
+                    np.random.shuffle(current_agents_generation) # occurs in place
+
+                    # play random pairs, works because of shuffle
+                    for i in range(0, len(current_agents_generation)-2, 2):
+                        row_player = current_agents_generation[i]
+                        col_player = current_agents_generation[i+1]
+                        game_obj = Game(row_player, col_player, row_payoff_matrix, col_payoff_matrix)
+                        while True:
+                            game_obj.step()
+                            if np.random.random() > gamma:
+                                # game is over between two agents
+                                break
+                        payoff[agent1_name] += game_obj.get_row_payoff()
+                        payoff[agent2_name] += game_obj.get_col_payoff()
+
+
 
 
     # for agent1_name, agent1 in agents:
